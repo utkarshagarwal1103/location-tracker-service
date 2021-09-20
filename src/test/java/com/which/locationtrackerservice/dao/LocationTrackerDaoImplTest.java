c231d96efd36b6963dcdf351fe4c12a3858fa2d1
utkarshagarwal1103 <utkarshagarwal1103@gmail.com>
2021-09-21 00:17:40 +0100

    updating dependencies

--- src/test/java/com/which/locationtrackerservice/dao/LocationTrackerDaoImplTest.java
+++ src/test/java/com/which/locationtrackerservice/dao/LocationTrackerDaoImplTest.java
@@ -3,28 +3,11 @@ package com.which.locationtrackerservice.dao;
 import com.which.locationtrackerservice.dao.impl.LocationTrackerDaoImpl;
 import com.which.locationtrackerservice.domain.GetDirectionList;
 import com.which.locationtrackerservice.exception.ServiceException;
-import com.which.locationtrackerservice.service.impl.LocationTrackerServiceImpl;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LocationTrackerDaoImplTest {
